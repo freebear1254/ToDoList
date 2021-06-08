e5b80ab1c6d0d0c7a3911003b4c464a3e16c1717
freebear1254 <freebear1254@gmail.com>
2021-06-08 19:51:33 +0900

    upload [idex.html/index.css/index.js]

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-const titles = document.getElementById('titles');
-titles.innerHTML = "Hi fuck"
+const titles = document.querySelector('#titles');
+const BASE_COLOR = "rgb(51, 136, 136)";
+const OTHER_COLOR ="#55aaaa";
+const CLICKED_CLASS = "clicked";
 
-console.log(titles);
-console.error("FUCK");
+
+// function titlesClick(){
+//     const currentColor = titles.style.color;
+//     if (currentColor === BASE_COLOR){
+//         titles.style.color = OTHER_COLOR;
+//     }else{
+//         titles.style.color = BASE_COLOR;
+//     }
+  
+// }
+
+// function titlesClick(){
+//     const currentClass = titles.className;
+//     if(currentClass !== CLICKED_CLASS){
+//         titles.className = CLICKED_CLASS;
+//     }else {
+//         titles.className = "";
+//     }
+// }
+
+// function titlesClick(){
+    // const hasClass = titles.classList.contains(CLICKED_CLASS);
+    // if(hasClass){
+        // titles.classList.remove(CLICKED_CLASS)
+    // }else {
+        // titles.classList.add(CLICKED_CLASS);
+    // }
+// }
+function titlesClick(){
+    titles.classList.toggle(CLICKED_CLASS);
+}
 
 function init(){
+    
+    titles.addEventListener("click", titlesClick);
     console.log("index.js");
 }
 
