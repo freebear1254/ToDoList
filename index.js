1019df6aa07e1f6105df204f40c757fd51305ac7
freebear1254 <freebear1254@gmail.com>
2021-06-08 09:37:25 +0900

    upload file [index.js]

---
+++ index.js
@@ -0,0 +1,12 @@
+const titles = document.getElementById('titles');
+titles.innerHTML = "Hi fuck"
+
+console.log(titles);
+console.error("FUCK");
+
+function init(){
+    console.log("index.js");
+}
+
+
+init();
