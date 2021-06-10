7fe14c40ef83e222cd66471aa88a52dd6e3fff1a
freebear1254 <freebear1254@gmail.com>
2021-06-11 07:17:23 +0900

    study javascript

--- app.js
+++ app.js
@@ -11,23 +11,23 @@
 
 // const calculator ={
 //     add : function(a,b){
-//         console.log (a+b);
+//         return a+b
 //     },
 //     minus : function(a,b){
-//         console.log(a-b);
+//        return a-b
 //     },
 //     divice : function(a,b){
-//         console.log(a/b);
+//        return a/b
 //     },
 //     dd : function(a,b){
-//         console.log(a**b);
+//        return a**b;
 //     }
 // }
 
 // calculator.add(10,20);
 // calculator.dd(10,1);
 
-const age = 92;
+const age =parseInt(prompt("How old"));
 
 function calculateKrAge(ageOfForeigner){
     return ageOfForeigner +2 ;
