5a60fac9a489811755f84b640782a3b066427377
freebear1254 <freebear1254@gmail.com>
2021-06-10 14:45:05 +0900

    going to work on the weeken

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
-const player = {
-    name : "kim",
-    point :10,
-    fat : false,
-};
+// const player = {
+//     name : "kim",
+//     point :10,
+//     fat : false,
+// };
 
+// const kk=["water","potato",'tomato'];
+// console.log(kk);
+// kk.push("one");
+// console.log(kk);
+
+// const calculator ={
+//     add : function(a,b){
+//         console.log (a+b);
+//     },
+//     minus : function(a,b){
+//         console.log(a-b);
+//     },
+//     divice : function(a,b){
+//         console.log(a/b);
+//     },
+//     dd : function(a,b){
+//         console.log(a**b);
+//     }
+// }
+
+// calculator.add(10,20);
+// calculator.dd(10,1);
+
+const age = 92;
+
+function calculateKrAge(ageOfForeigner){
+    return ageOfForeigner +2 ;
+}
+
+const krAge = calculateKrAge(age);
+console.log(krAge);
