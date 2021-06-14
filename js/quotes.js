ffd5887d1a164faf5dadd9390ec213f5a7341423
freebear1254 <freebear1254@gmail.com>
2021-06-14 16:07:41 +0900

    create new file [background.js]

--- js/quotes.js
+++ js/quotes.js
@@ -1,3 +1,54 @@
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+
 const quotes = [
-    {}
+    {
+        quote : "quote01",
+        author : "author01"
+    },
+    {
+        quote : "quote02",
+        author : "author02"
+    },
+    {
+        quote : "quote03",
+        author : "author03"
+    },
+    {
+        quote : "quote04",
+        author : "author04"
+    },
+    {
+        quote : "quote05",
+        author : "author05"
+    },
+    {
+        quote : "quote06",
+        author : "author06"
+    },
+    {
+        quote : "quote07",
+        author : "author07"
+    },
+    {
+        quote : "quote08",
+        author : "author08"
+    },
+    {
+        quote : "quote09",
+        author : "author09"
+    },
+    {
+        quote : "quote10",
+        author : "author10"
+    },
 ]
+
+function init(){
+    console.log("quotes.js");
+    const todayQuote = quotes[Math.floor(Math.random()*quotes.length)];
+    quote.innerText =todayQuote.quote;    
+    author.innerText =todayQuote.author;    
+}
+init();
