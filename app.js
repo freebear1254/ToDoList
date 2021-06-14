5dc180a7a459156308c0c2ffaae81444d46ad60a
freebear1254 <freebear1254@gmail.com>
2021-06-14 13:21:13 +0900

    update login form

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const hello = document.querySelector("#hello");
+const localUserName = localStorage.getItem("userName");
 
-const CLASSNAME_HIDDEN = "hidden"
+
+const CLASSNAME_HIDDEN = "hidden";
+const userNameKey ="userName";
+const saveUserName = localStorage.getItem(userNameKey);
+
+
+function paintHello(username){
+    hello.innerHTML = `Hello ${username}`;
+    hello.classList.remove(CLASSNAME_HIDDEN);
+}
+
+if(saveUserName === null){
+ loginForm.classList.remove(CLASSNAME_HIDDEN);   
+ loginForm.addEventListener("submit",onLoginSubmit);
+}else {
+    paintHello(saveUserName);
+}
+    
 
 function onLoginSubmit(event){
     event.preventDefault();
     const userName = loginInput.value;
-    loginForm.classList.add(CLASSNAME_HIDDEN);
-    hello.innerHTML =  `Hello ${userName}`;
-    hello.classList.remove(CLASSNAME_HIDDEN);
+    loginForm.classList.add(CLASSNAME_HIDDEN);    
+    localStorage.setItem(userNameKey,userName);
+    paintHello(userName);
 
 }
 
-loginForm.addEventListener("submit",onLoginSubmit);
+
+function init(){
+    
+}
+
