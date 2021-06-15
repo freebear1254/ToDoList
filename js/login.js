64e1b9189501d6aa93c358bb662dc3b2645edc2e
freebear1254 <freebear1254@gmail.com>
2021-06-15 12:32:24 +0900

    update localStorage

--- js/login.js
+++ js/login.js
@@ -33,4 +33,5 @@ function onLoginSubmit(event){
 function init(){
     
 }
+init();
 
--- js/todo.js
+++ js/todo.js
@@ -2,25 +2,55 @@ const todoForm = document.getElementById("todo-form");
 const todoList = document.getElementById("todo-list");
 const todoInput = todoForm.querySelector("input");
 const TODOLIST = "todoList";
+const savedTodos = JSON.parse(localStorage.getItem(TODOLIST));
+const toDos=[];
 
 
-function paintTodo(newTodo){
+function writeTodo() {
+    if(savedTodos !== null){
+        for(i=0;i<savedTodos.length; i++){
+            toDos.push(savedTodos[i]);
+        }        
+         for (i = 0; i < toDos.length; i++) {
+      paintTodo(toDos[i]);
+        }
+    }   
+}
+
+function saveTodos() {
+    localStorage.setItem(TODOLIST, JSON.stringify(toDos));
+}
+
+function deleteTodo(evnet) {
+    const deleteTarget = event.target.parentElement;
+    deleteTarget.remove();    
+}
+
+function paintTodo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    const button = document.createElement("button");
+    const delBtn = document.createElement("button");
     span.innerText = newTodo;
-    button.innerText ="❌";
+    delBtn.innerText = "❌";
     li.appendChild(span);
-    li.appendChild(button);
+    li.appendChild(delBtn);
     todoList.appendChild(li);
-
-   
+    delBtn.addEventListener("click", deleteTodo);
 }
 
-function handleTodoSubmit(event){
+
+function handleTodoSubmit(event) {
     event.preventDefault();
     const newTodo = todoInput.value;
-    todoInput.value ="";
+    todoInput.value = "";
+    toDos.push(newTodo);
+    saveTodos();
     paintTodo(newTodo);
 }
-todoForm.addEventListener("submit",handleTodoSubmit)
+
+function init() {
+    todoForm.addEventListener("submit", handleTodoSubmit);
+    writeTodo();
+}
+
+init();
